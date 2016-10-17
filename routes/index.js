55a1c996516e1959e764ffb483bb418a258d672d
Gaston Beaucage <Gaston.E.Beaucage@gmail.com>
2016-10-16 21:54:47 -0700

    added message routes and custom email message

--- routes/index.js
+++ routes/index.js
@@ -7,57 +7,57 @@ var nodemailer = require("nodemailer");
 var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
 
 router.get('/', function (req, res) {
-    res.render('index', { user : req.user });
+	res.render('index', { user : req.user });
 });
 
 router.get('/register', function(req, res) {
-    res.render('register', { });
+	res.render('register', { });
 });
 
 router.post('/register', function(req, res, next) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-        if (err) {
-          return res.render('register', { error : err.message });
-        }
+	Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+		if (err) {
+		  return res.render('register', { error : err.message });
+		}
 
-        passport.authenticate('local')(req, res, function () {
-            req.session.save(function (err) {
-                if (err) {
-                    return next(err);
-                }
-                res.redirect('/');
-            });
-        });
-    });
+		passport.authenticate('local')(req, res, function () {
+			req.session.save(function (err) {
+				if (err) {
+					return next(err);
+				}
+				res.redirect('/');
+			});
+		});
+	});
 });
 
 
 router.get('/login', function(req, res) {
-    res.render('login', { user : req.user, error : req.flash('error')});
+	res.render('login', { user : req.user, error : req.flash('error')});
 });
 
 router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), function(req, res, next) {
-    req.session.save(function (err) {
-        if (err) {
-            return next(err);
-        }
-        res.redirect('/');
-    });
+	req.session.save(function (err) {
+		if (err) {
+			return next(err);
+		}
+		res.redirect('/');
+	});
 });
 
 router.get('/logout', function(req, res, next) {
-    req.logout();
-    req.session.save(function (err) {
-        if (err) {
-            return next(err);
-        }
-        res.redirect('/');
-    });
+	req.logout();
+	req.session.save(function (err) {
+		if (err) {
+			return next(err);
+		}
+		res.redirect('/');
+	});
 });
 
 router.get('/cheer', function(req, res) {
 	Account.find({}, function(err, users) {
-	  if (err) throw err;
+	if (err) throw err;
 	var usernames = [];
 	users.forEach(function(user) {
 		usernames.push(user.username);
@@ -67,8 +67,7 @@ router.get('/cheer', function(req, res) {
 });
 
 router.post('/cheer', function(req, res) {
-	console.log(req.body.username);
-	console.log(req.body.text);
+	console.log(Account);
 	var transporter = nodemailer.createTransport("SMTP",{
 	   service: "Gmail",  // sets automatically host, port and connection security settings
 	   auth: {
@@ -80,9 +79,9 @@ router.post('/cheer', function(req, res) {
 	var mailOptions = {
 		from: '"John Doe" <nodemailercomp3900@gmail.com>', // sender address
 		to: 'Gaston.E.Beaucage@gmail.com', // list of receivers
-		subject: 'Hello ✔', // Subject line
-		text: 'Hello world 🐴', // plaintext body
-		html: '<b>Hello world 🐴</b>' // html body
+		subject: 'Compliment from' + req.body.username, // Subject line
+		text: 'Thank you!', // plaintext body
+		html: '<b>'+ req.body.text +'</b>' // html body
 	};
 
 	transporter.sendMail(mailOptions, function(error, info){
@@ -94,7 +93,7 @@ router.post('/cheer', function(req, res) {
 });
 
 router.get('/ping', function(req, res){
-    res.status(200).send("pong!");
+	res.status(200).send("pong!");
 });
 
 module.exports = router;
